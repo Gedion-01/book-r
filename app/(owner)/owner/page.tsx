ceb1edde0ec11e5c12e406f2eb51d4b8e0fa7303
gedion <117908662+Gedion-01@users.noreply.github.com>
2024-08-04 00:04:32 +0300

    update

---
+++ app/(owner)/owner/page.tsx
@@ -0,0 +1,12 @@
+import AuthForm from "@/components/auth-form";
+
+interface PageProps {
+  searchParams: {
+    mode: "login" | "sign-up";
+  };
+}
+
+export default function Page({ searchParams }: PageProps) {
+  const formMode = searchParams.mode || "login";
+  return <AuthForm mode={formMode} role="user" title={formMode === "login" ? "Login as Book Owner" : "Signup as Book Owner"} />;
+}
